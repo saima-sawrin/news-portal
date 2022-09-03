944934beb4e53eceff7649a0a845937d7a1ae5f7
Saima <saimasawrin324@gmail.com>
2022-09-03 12:42:46 +0600

    Categories added

--- index.js
+++ index.js
@@ -19,12 +19,34 @@ const displayCategories= categories =>{
     });
 
 }
-const loadNews = code =>{
-    const url = ``
+const loadNews =async() =>{
+    const url = `https://openapi.programming-hero.com/api/news`
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data.news_category);
+    displayNews(data.data);
         
+}
+const displayNews = newsDetails =>{
+    const newsContainer = document.getElementById('news-container');
+    newsDetails.forEach(News =>  {
+     const newsDiv = document.createElement('div');
+    newsDiv.innerHTML = `
+          <div class="col-md-4 mt-2">
+          <img src="${News.rating.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">${News.rating.title}</h5>
+            <p class="card-text">${News.details}</p>
+            <p class="card-text">${News.details}<small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+                 
+                            
+    `;
+    categoriesContainer.appendChild(categoryInput);
+        
+    });
 }
  
 loadCategories();
