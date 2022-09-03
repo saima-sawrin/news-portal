f3ef25183529289e18cefd2bbd268d90c74121c2
Saima <saimasawrin324@gmail.com>
2022-09-04 01:48:05 +0600

    news feed section added

--- index.js
+++ index.js
@@ -9,29 +9,23 @@ const loadCategories = () =>{
     
 }
     const displayCategories= categories =>{
+        toggleSpinner(true);
     const categoriesContainer = document.getElementById('categoriesContainer');
     categories.forEach(category => {
         const categoryInput = document.createElement('label');
     categoryInput.innerHTML = `
-                 <label class="btn d-inline mx-4 ps-3 pe-3" onclick ="loadNews()">${category.category_name}</label>
+                 <label id="${category.category_id}" class="btn d-inline mx-4 ps-3 pe-3" onclick ="loadNews(this.id)">${category.category_name}</label>
                                
     `;
     categoriesContainer.appendChild(categoryInput);
         
     });
+    
 
 }
-// News Details
 
-// const loadNews = async(newsId) =>{
-//     const url = `https://openapi.programming-hero.com/api/news=${newsId}`
-//     const res = await fetch(url)
-//     const data = await res.json()
-//     displayNews(data);
-
-// }
-const loadNews = (id) => { 
-    fetch ( `https://openapi.programming-hero.com/api/news/category/03` ) 
+const loadNews = id => { 
+    fetch ( `https://openapi.programming-hero.com/api/news/category/${id}` ) 
     . then ( res => res . json ()) 
     . then ( newsData => displayNews ( newsData ))
     .catch((e) => {
@@ -41,6 +35,7 @@ const loadNews = (id) => {
         const displayNews = newsData =>{
    
        const newsContainer = document.getElementById('news-container');
+       newsContainer.textContent = '';
        // console.log(newsData);
        for ( const allNews of newsData.data )
        {
@@ -51,25 +46,25 @@ const loadNews = (id) => {
         <div class="card mb-3 mt-5 mx-5" >
          <div class="row g-0"> 
          <div class="col-md-3">
-          <img src=" ${allNews.image_url} " class="img-fluid rounded- start px-3" alt="..."> 
+          <img src=" ${allNews.image_url} " class="img-fluid rounded- start px-3 w-100 h-100 " alt="..."> 
           </div>
            <div class="col-md-8"> 
            <div class="card-body">
             <h5 class="card-title">${allNews.title}</h5> 
-            <p class="card-text"> ${allNews.details}</p>
+            <p class="card-text"> ${allNews.details.slice(0, 400)+ "......."}</p>
             <div class ="d-flex justify-content-around">
-            <div >
+            <div>
             <img id ="authorImg" class="rounded mx-auto" src ="${allNews.author.img}"> <small class="text-muted">
             <p class="card-text"> ${allNews.author.name}</small></p> 
             <p class="card-text"> ${allNews.author.published_date}</small></p> 
             </div>
             <div>
-            <img src ="image/eye.png"> <small class="text-muted">
-            <p class="card-text> ${allNews.total_view }</p>
+            <img src ="image/eye.png"> 
+            <p class="card-text> <small class="text-muted"> ${allNews.total_view }</small></p>
             </div>
             <div>
             <img src ="image/rating.png"> <small class="text-muted">
-            <p> ${allNews.rating.badge}  <br> ${allNews.rating.number}  </p>
+            <p> ${allNews.rating.badge}  <br> ${allNews.rating.number} </small>  </p>
             </div>
             </div>
             
@@ -96,6 +91,6 @@ const toggleSpinner = isLoading => {
 }
 
 
-// loadNews();
+
  
 loadCategories();
