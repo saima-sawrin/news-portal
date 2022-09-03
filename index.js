2ea3f9a0ae80eb515895781493898b7960804851
Saima <saimasawrin324@gmail.com>
2022-09-03 23:28:59 +0600

    news details added

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ const loadCategories = () =>{
     
 }
     const displayCategories= categories =>{
-    // console.log(categories);
     const categoriesContainer = document.getElementById('categoriesContainer');
     categories.forEach(category => {
         const categoryInput = document.createElement('label');
     categoryInput.innerHTML = `
-                 <label class="btn d-inline mx-4 ps-3 pe-3" onclick ="loadNews(${category.category_id})">${category.category_name}</label>
+                 <label class="btn d-inline mx-4 ps-3 pe-3" onclick ="loadNews()">${category.category_name}</label>
                                
     `;
     categoriesContainer.appendChild(categoryInput);
@@ -22,37 +21,51 @@ const loadCategories = () =>{
     });
 
 }
+// News Details
 
-const loadNews = async(newsId) =>{
-    const url = `https://openapi.programming-hero.com/api/news=${newsId}`
-    const res = await fetch(url)
-    const data = await res.json()
-    displayNews(data);
+// const loadNews = async(newsId) =>{
+//     const url = `https://openapi.programming-hero.com/api/news=${newsId}`
+//     const res = await fetch(url)
+//     const data = await res.json()
+//     displayNews(data);
 
-}
-
-const displayNews = newsData =>{
-    for(const allNews of newsData.data){
-        console.log(allNews.rating);
-    }
-    // const newsContainer = document.getElementById('news-container');
-    // console.log(totalNews);
-    // totalNews.forEach(News.data =>{
-    //     console.log(News.rating);
-        // const newsDiv =  document.createElement('div');
-        // newsDiv.innerHTML = `
-        // <div class="col-md-4 mt-2">
-         
-        //     <h5 class="title">Nothing</h5>
-           
-        //   </div>
-        // </div>
+// }
+const loadNews = () => { 
+    fetch ( 'https://openapi.programming-hero.com/api/news/category/01' ) 
+    . then ( res => res . json ()) 
+    . then ( newsData => displayNews ( newsData ))
+    .catch((e) => {
+        console.log('Error:', e);
+    });
+ }
+        const displayNews = newsData =>{
+   
+       const newsContainer = document.getElementById('news-container');
+       // console.log(newsData);
+       for ( const allNews of newsData.data )
+       {
+        // console.log(News.rating);
+        const newsDiv =  document.createElement('div');
+        newsDiv.classList.add('news-item');
+        newsDiv.innerHTML = `
+        <div class="card mb-3" >
+         <div class="row g-0"> 
+         <div class="col-md-4">
+          <img src=" ${allNews.thumbnail_url} " class="img-fluid rounded- start" alt="..."> </div>
+           <div class="col-md-8"> 
+           <div class="card-body">
+            <h5 class="card-title">${allNews.title}</h5> 
+            <p class="card-text"> ${allNews.details}</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> 
+            </div> 
+            </div> 
+            </div> 
+            </div>
                 
-        // `;
-        // newsContainer.appendChild(newsDiv);
+        `;
+        newsContainer.appendChild(newsDiv);
     }
-           
-
+}
 
 // loadNews();
  
