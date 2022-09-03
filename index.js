426cffac63a8946ef99d201b54e967b1dc2ba5bd
Saima <saimasawrin324@gmail.com>
2022-09-03 18:23:39 +0600

    modal added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const loadCategories = async() =>{
+const loadCategories = async(category_id) =>{
     const url = `https://openapi.programming-hero.com/api/news/categories`
     const res = await fetch(url);
     const data = await res.json();
@@ -10,7 +10,7 @@ const displayCategories= categories =>{
     categories.forEach(category => {
         const categoryInput = document.createElement('label');
     categoryInput.innerHTML = `
-                 <label class="btn d-inline mx-4 ps-3 pe-3" onclick ="loadNews(${category.category_name})">${category.category_name}</label>
+                 <label class="btn d-inline mx-4 ps-3 pe-3" onclick ="loadNews('${category.category_id}')">${category.category_name}</label>
                  
                             
     `;
@@ -19,34 +19,36 @@ const displayCategories= categories =>{
     });
 
 }
-const loadNews =async() =>{
-    const url = `https://openapi.programming-hero.com/api/news`
-    const res = await fetch(url);
-    const data = await res.json();
-    displayNews(data.data);
+const loadNews =async(id) =>{
+    const url = `https://openapi.programming-hero.com/api/news=${id}`
+     fetch(url)
+    .then (res=> res.json())
+    .then (data => displayNews(data[0].data))
         
 }
-const displayNews = newsDetails =>{
+const displayNews = News =>{
     const newsContainer = document.getElementById('news-container');
-    newsDetails.forEach(News =>  {
-     const newsDiv = document.createElement('div');
-    newsDiv.innerHTML = `
+   
+        // console.log(News);
+     
+    newsContainer.innerHTML = `
           <div class="col-md-4 mt-2">
           <img src="${News.rating.thumbnail_url}" class="img-fluid rounded-start" alt="...">
         </div>
         <div class="col-md-8">
           <div class="card-body">
             <h5 class="card-title">${News.rating.title}</h5>
-            <p class="card-text">${News.details}</p>
-            <p class="card-text">${News.details}<small class="text-muted">Last updated 3 mins ago</small></p>
+            <p class="card-text">${News.author.details}</p>
+            <p class="card-text">${News.author.details}<small class="text-muted">Last updated 3 mins ago</small></p>
           </div>
         </div>
                  
                             
     `;
-    categoriesContainer.appendChild(categoryInput);
+   
+   
         
-    });
+
 }
  
 loadCategories();
